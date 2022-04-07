78d039963fb8e4ae054fec78d11aa71d5fa26a15
Meet Rajput <rajputmeet10@gmail.com>
2022-04-07 22:58:44 +0530

    frontPage done

---
+++ src/components/front.js
@@ -0,0 +1,33 @@
+import React, {useState, useEffect } from 'react';
+import Fade from 'react-reveal/Fade';
+import './front.css';
+import Type from './type';
+const Header = () => {
+  return (
+      <div className="mainBody">
+        <Fade left={true} duration={1000} delay={500} distance="30px">
+          <h1>
+            <div className='intro_lines'>
+                {'Hi, my name is'}{' '}
+                <span className='name'>{'Meet Rajput'}</span>
+            </div>
+            <br />
+            <div style={{textAlign: "center" }}>
+                <Type />
+            </div>
+          </h1>
+        </Fade>
+        <Fade left={true} duration={1000} delay={1000} distance="30px">
+          <p>
+          <button  href="https://twitter.com/kamildyrek">
+                <div className="left" />
+                Download my CV!
+                <div className="right" />
+            </button>
+          </p>
+        </Fade>
+      </div>
+  );
+};
+
+export default Header;
---
+++ src/components/type.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+function Type() {
+  return (
+    <Typewriter
+      options={{
+        strings: [
+          "Software Developer",
+          "Deep Learning Engineer",
+          "Android Developer",
+          "Competitive Programmer",
+        ],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
+      }}
+    />
+  );
+}
+
+export default Type;
