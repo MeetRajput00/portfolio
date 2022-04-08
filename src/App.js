ff133ec8c735ee68ee3fa3911e67432aef8bd3ad
Meet Rajput <rajputmeet10@gmail.com>
2022-04-08 12:04:32 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import Header from './components/front';
 import SideBar from './components/sidebar';
 import React from 'react';
 import { Menu } from '@styled-icons/feather/Menu';
-
+import About from './components/about';
+import Contact from './components/contact';
+import Projects from './components/projects';
+import Skills from './components/skills';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function App() {
   const [on, setOn] = React.useState(false);
 
@@ -18,7 +22,15 @@ function App() {
         </a>
       </aside>
       {on && <SideBar openClass="open" />}
-      <Header/>
+      <Router>
+          <Routes>
+            <Route path="/" element={<Header/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/contact" element={<Contact/>}/>
+            <Route path="/projects" element={<Projects/>}/>
+            <Route path="/skills" element={<Skills/>}/>
+          </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/skills.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Skills() {
+  return (
+    <div>skills</div>
+  )
+}
+
+export default Skills
