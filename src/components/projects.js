826eded6d3750306d9591a5cba53ba0476f95352
Meet Rajput <rajputmeet10@gmail.com>
2022-04-14 10:39:52 +0530

    front-end done

--- src/components/projects.js
+++ src/components/projects.js
@@ -14,8 +14,8 @@ function Projects() {
         <h1>Recent Projects</h1>
       </div>
     <div className='projectsBody'>
-      <Card githubLink="https://github.com/MeetRajput00/discord-clone-final" imageLocation={discordSS} projectTitle="Discord Clone using React and Redux"/>
-      <Card githubLink="https://github.com/MeetRajput00/network-traffic-analyzer" imageLocation={ntaSS} projectTitle="Network Traffic Analyzer using C#"/>
+      <Card githubLink="https://github.com/MeetRajput00/discord-clone-final" imageLocation={discordSS} projectTitle="Discord Clone using React"/>
+      <Card githubLink="https://github.com/MeetRajput00/network-traffic-analyzer" imageLocation={ntaSS} projectTitle="Network Traffic Analyzer"/>
       <Card githubLink="https://github.com/MeetRajput00/Face-Detection" imageLocation={faceDetectionSS} projectTitle="Face Detection using OpenCV"/>
     </div>
     <div className='projectsBody'>
